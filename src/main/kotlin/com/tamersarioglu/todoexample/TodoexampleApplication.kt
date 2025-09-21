eb19aa73d0138a2a2cfac1123cc26638ba341e0f
Tamer Sarioglu <tamer.sarioglu@mobillium.com>
2025-09-21 18:09:28 +0300

    initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/main/kotlin/com/tamersarioglu/todoexample/TodoexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.tamersarioglu.todoexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoexampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoexampleApplication>(*args)
+}
